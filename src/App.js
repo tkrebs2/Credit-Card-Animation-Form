f1d678f0cf66227f82ad1263f63c4f2e9e6ff28c
Tanner <tannerkrebs2@gmail.com>
2020-05-18 13:54:54 -0400

    removed some unnecessary css and add yup package to package.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Formik, Field } from "formik";
+import * as Yup from "yup";
+
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        initialValues={{
+          cardNumber: "",
+          cardholderName: "",
+          cvv: "",
+          expirationMonth: "",
+          expirationDay: ""
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit} style={{ width: "800px" }}>
+            <input
+              type="number"
+              name="cardNumber"
+              placeholder="Card Number"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.cardNumber}
+            />
+            {errors.cardNumber && touched.cardNumber && errors.cardNumber}
+            <input
+              type="text"
+              name="cardholderName"
+              placeholder="Card Holder Name"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.cardholderName &&
+              touched.cardholderName &&
+              errors.cardholderName}
+            <input
+              type="number"
+              name="cvv"
+              placeholder="CVV"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.cvv}
+            />
+            {errors.cvv && touched.cvv && errors.cvv}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
